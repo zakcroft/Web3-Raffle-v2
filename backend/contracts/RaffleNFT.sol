50cb4195b3002db94aaabd3c92ae9442e6ee83cc
zak <zakcroft@gmail.com>
2023-03-06 08:28:39 +0000

    RaffleNFT

---
+++ backend/contracts/RaffleNFT.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract BasicNft is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private s_tokenCounter;
+    string public constant TOKEN_URI =
+        "ipfs://bafybeifdj4gp7bxo4zd2jh6h6al4etoa3fcifhxgpk5pafquxxhevhut24/?filename=happy.json";
+
+    constructor() ERC721("Smile", "HAPPY WINNER") {
+    }
+
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter.current());
+        s_tokenCounter.increment();
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        // require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        return TOKEN_URI;
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter.current();
+    }
+}
---
+++ backend/deploy/02-deploy-nft.js
@@ -0,0 +1,25 @@
+const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    log("----------------------------------------------------")
+    const args = []
+    const raffleNFT = await deploy("RaffleNFT", {
+        from: deployer,
+        args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    // Verify the deployment
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(raffleNFT.address, args)
+    }
+}
+
+module.exports.tags = ["all", "RaffleNFT"]
